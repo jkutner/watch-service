4b27be2522c38ddefb48236f882cafa8386b40f9
Joe Kutner <jpkutner@gmail.com>
2017-04-28 14:38:20 -0500

    first

---
+++ src/main/java/Main.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.sql.*;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+public class Main extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    if (req.getRequestURI().endsWith("/db")) {
+      showDatabase(req,resp);
+    } else {
+      showHome(req,resp);
+    }
+  }
+
+  private void showHome(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    resp.getWriter().print("Hello from Java!");
+  }
+
+  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    try {
+      Connection connection = DatabaseUrl.extract().getConnection();
+
+      Statement stmt = connection.createStatement();
+      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+      String out = "Hello!\n";
+      while (rs.next()) {
+          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
+      }
+
+      resp.getWriter().print(out);
+    } catch (Exception e) {
+      resp.getWriter().print("There was an error: " + e.getMessage());
+    }
+  }
+
+  public static void main(String[] args) throws Exception{
+    final Thread mainThread = Thread.currentThread();
+    Runtime.getRuntime().addShutdownHook(new Thread() {
+      public void run() {
+        System.out.println("Goodbye world");
+        try {
+          // mainThread.join();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+
+    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+    context.addServlet(new ServletHolder(new Main()),"/*");
+    server.start();
+    server.join();
+  }
+}
