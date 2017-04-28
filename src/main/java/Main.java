7b91185c18e7f7c0985a0902372dbb81e7eb8d1f
Joe Kutner <jpkutner@gmail.com>
2017-04-28 17:33:29 -0500

    Added file watcher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,39 @@
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.sql.*;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
-import com.heroku.sdk.jdbc.DatabaseUrl;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
 
 public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
+    PrintWriter writer = new PrintWriter("work/foobar.txt", "UTF-8");
+    writer.println("The first line");
+    writer.close();
 
-    if (req.getRequestURI().endsWith("/db")) {
-      showDatabase(req,resp);
-    } else {
-      showHome(req,resp);
-    }
-  }
-
-  private void showHome(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
     resp.getWriter().print("Hello from Java!");
   }
 
-  private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
-      throws ServletException, IOException {
-    try {
-      Connection connection = DatabaseUrl.extract().getConnection();
-
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      String out = "Hello!\n";
-      while (rs.next()) {
-          out += "Read from DB: " + rs.getTimestamp("tick") + "\n";
-      }
-
-      resp.getWriter().print(out);
-    } catch (Exception e) {
-      resp.getWriter().print("There was an error: " + e.getMessage());
-    }
-  }
-
   public static void main(String[] args) throws Exception{
-    final Thread mainThread = Thread.currentThread();
+    final WatchService watchService = FileSystems.getDefault().newWatchService();
+    Path path = Paths.get(args.length > 0 ? args[0] : "work/");
+    path.register(watchService,ENTRY_CREATE,ENTRY_DELETE,ENTRY_MODIFY);
+
     Runtime.getRuntime().addShutdownHook(new Thread() {
       public void run() {
         System.out.println("Goodbye world");
         try {
-          // mainThread.join();
+          watchService.close();
         } catch (Exception e) {
           e.printStackTrace();
         }
@@ -66,6 +46,23 @@ public class Main extends HttpServlet {
     server.setHandler(context);
     context.addServlet(new ServletHolder(new Main()),"/*");
     server.start();
-    server.join();
+
+    while (true) {
+      try {
+        WatchKey key = watchService.take();
+        if (key != null) {
+          for (WatchEvent<?> event : key.pollEvents()) {
+            System.out.println("Received watch event: " + event.context().toString());
+          }
+          boolean reset = key.reset();
+          if (!reset) {
+            System.out.println("Could not reset the watch key.");
+            break;
+          }
+        }
+      } catch (ClosedWatchServiceException e) {
+        // do nothing
+      }
+    }
   }
 }
--- start.sh
+++
@@ -1,1 +0,0 @@
-java $JAVA_OPTS -cp target/classes:target/dependency/* Main
